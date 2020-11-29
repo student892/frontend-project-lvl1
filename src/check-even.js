74aceff1d1387bfcd29a52e47518efb9e2df1079
student892 <relsqwe@mail.ru>
2020-11-30 00:02:56 +0300

    adding check-even.js

--- src/check-even.js
+++ src/check-even.js
@@ -6,10 +6,22 @@ export const checkEven = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const number = [15, 6, 7, 20, 11, 14, 3];
+  const numbers = [15, 6, 7, 20, 11, 14, 3];
   for(let i = 0; i <= 3; i = i + 1) {
     if (i === 3) {
       console.log(`Congratulations, ${name}`);
+      break;
+    }
+    let currentNumber = numbers[i];
+    console.log(`Question: ${currentNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const correctAnswer = currentNumber % 2 === 0 ? 'yes' : 'no';
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
+      console.log(`Let's try again, ${name}!`)
+      break;
     }
   }
 };
