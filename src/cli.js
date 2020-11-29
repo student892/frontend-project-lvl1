50331e551353eb77534bc20c63b43a8e1181b480
student892 <relsqwe@mail.ru>
2020-11-29 17:23:59 +0300

    integrating eslint

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+
 export const askUser = () => {
 	const name = readlineSync.question('May I have your name? ');
-	console.log('Hello ' + name + '!');
-}
-
+	console.log(`Hello ${name}!`);
+};
