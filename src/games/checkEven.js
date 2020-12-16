8557d02daf454f69eb89f64e21607d0d64ed2fef
student892 <relsqwe@mail.ru>
2020-12-16 21:54:46 +0300

    correction in checkEven

--- src/games/checkEven.js
+++ src/games/checkEven.js
@@ -1,14 +1,14 @@
 import getRandomNumber from '../getRandomNumber.js';
 
 const isEven = (number) => {
-  const answer = number % 2 === 0 ? 'yes' : 'no';
+  const answer = number % 2 === 0;
   return answer;
 };
 const gameFunction = () => {
   const gameStartMessage = 'Answer "yes" if the number is even, otherwise answer "no".';
   const currentNumber = getRandomNumber(1, 100);
   const question = `${currentNumber}`;
-  const correctAnswer = isEven(currentNumber);
+  const correctAnswer = isEven(currentNumber) ? 'yes' : 'no';
   return { gameStartMessage, question, correctAnswer };
 };
 export default gameFunction;
